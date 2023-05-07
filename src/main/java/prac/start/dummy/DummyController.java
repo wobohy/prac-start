77e52af472833673bdda60d4b876724008f431d6
wb <wubohy@hanmail.net>
2023-05-07 23:07:03 +0900

    feat: main.html 매핑 refactor: 미사용 class 삭제, service 추가

--- src/main/java/prac/start/dummy/DummyController.java
+++ src/main/java/prac/start/dummy/DummyController.java
@@ -1,47 +1,25 @@
 package prac.start.dummy;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/dummy")
 @RestController
 public class DummyController {
-    private final RestTemplate restTemplate;
-
-    private final HttpHeaders headers = DummyUtil.getHttpHeaders();
+    private final DummyService dummyService;
 
     @Autowired
-    public DummyController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public DummyController (DummyService dummyService) {
+        this.dummyService = dummyService;
     }
 
-    public String getData(String apiUrl) {
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
-
-        ResponseEntity<String> response;
-        try {
-             response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
-        } catch (Exception e) {
-            return "[exception]" + apiUrl + e + "fail";
-        }
-
-        if (HttpStatus.OK == response.getStatusCode()) {
-            String responseBody = response.getBody();
-            System.out.println("responseBody, " + responseBody);
-            return responseBody;
-        } else {
-            // API 호출 실패 처리 코드
-            return "getDummy fail";
-        }
-    }
-
-    @GetMapping("/dummy")
-    public String test() {
-
-        return "/dummy/dummy";
+    @GetMapping("/get/{step}")
+    public String getDummy(/*@PathVariable("id") String id, */@PathVariable("step") String step) {
+        System.out.println("step = " + step);
+        String step1 = dummyService.isStep(step);
+        return dummyService.getData("", "");
     }
 }
---
+++ src/main/java/prac/start/dummy/DummyService.java
@@ -0,0 +1,55 @@
+package prac.start.dummy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class DummyService {
+    private final RestTemplate restTemplate;
+    private final String APP_ID = "6451014824116412c42378e8";
+    private final String API_URL = "https://dummyapi.io/data/v1/";
+
+    @Autowired
+    public DummyService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getData(String id, String apiUrl) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("app-id", APP_ID);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+        } catch (Exception e) {
+            return "[exception]" + apiUrl + e + "fail";
+        }
+
+        if (HttpStatus.OK == response.getStatusCode()) {
+            String responseBody = response.getBody();
+            System.out.println("responseBody, " + responseBody);
+            return responseBody;
+        } else {
+            // API 호출 실패 처리 코드
+            return "getDummy fail";
+        }
+    }
+
+    public String isStep (String step) {
+        String[] steps = {"user", "post", "comment", "tag"};
+        List<String> stepList = Arrays.asList(steps);
+
+        if (!stepList.contains(step)) {
+            System.out.println("this step is not valid");
+        }
+
+        return step;
+    }
+
+}
